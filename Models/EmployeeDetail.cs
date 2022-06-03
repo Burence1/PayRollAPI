d1ce0b4f00ca9adbb036c754ee7b4f4855237332
Burence1 <burensomondi@gmail.com>
2022-06-03 12:17:39 +0300

    initial commit

---
+++ Models/EmployeeDetail.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PayrollAPI.Models
+{
+    public class EmployeeDetail
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
+        public int EmployeeNo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Citizenship { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public int IdNo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string KRANo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NHIFNo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NSSFNo { get; set; }
+
+        [Required]
+        [Column(TypeName = "Decimal (38,2)")]
+        public Decimal GrossPay { get; set; }
+
+        [Required]
+        public int NumberOfChildren { get; set; }
+
+        [StringLength(50)]
+        public string MaritalStatus { get; set; }
+
+        [Required]
+        public int EmployerCode { get; set; }
+
+
+    }
+}
