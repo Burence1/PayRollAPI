2693f844e8eedfeb59c3268f1c6366d8d22c6494
bomondi <bomondi@fintech-group.com>
2023-01-23 21:54:18 +0300

    add stored procedures

--- DBInterface/DBInterface.cs
+++ DBInterface/DBInterface.cs
@@ -25,7 +25,7 @@ public class DBInterface
             MyCmd.Parameters.Add("@mode", SqlDbType.Int).Value = mode;
             MyCmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = employeeDetail.FirstName;
             MyCmd.Parameters.Add("@Citizenship", SqlDbType.VarChar).Value = employeeDetail.Citizenship;
-            MyCmd.Parameters.Add("@DateOfBirth", SqlDbType.VarChar).Value = employeeDetail.Citizenship;
+            MyCmd.Parameters.Add("@DateOfBirth", SqlDbType.VarChar).Value = employeeDetail.DateOfBirth;
             MyCmd.Parameters.Add("@EmployeeEducations", SqlDbType.VarChar).Value = employeeDetail.EmployeeEducations;
             MyCmd.Parameters.Add("@EmployeeId", SqlDbType.VarChar).Value = employeeDetail.EmployeeId;
             MyCmd.Parameters.Add("@EmployeeNo", SqlDbType.VarChar).Value = employeeDetail.EmployeeNo;
@@ -34,8 +34,12 @@ public class DBInterface
             MyCmd.Parameters.Add("@GrossPay", SqlDbType.VarChar).Value = employeeDetail.GrossPay;
             MyCmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = employeeDetail.LastName;
             MyCmd.Parameters.Add("@KRANo", SqlDbType.VarChar).Value = employeeDetail.KRANo;
-            MyCmd.Parameters.Add("@Mnth", SqlDbType.VarChar).Value = employeeDetail.;
-            MyCmd.Parameters.Add("@EmployeeNo", SqlDbType.VarChar).Value = EmployeeNo;
+            MyCmd.Parameters.Add("@MaritalStatus", SqlDbType.VarChar).Value = employeeDetail.MaritalStatus;
+            MyCmd.Parameters.Add("@PhoneNumber;", SqlDbType.VarChar).Value = employeeDetail.PhoneNumber;
+            MyCmd.Parameters.Add("@NSSFNo;", SqlDbType.VarChar).Value = employeeDetail.NSSFNo;
+            MyCmd.Parameters.Add("@NHIFNo;", SqlDbType.VarChar).Value = employeeDetail.NHIFNo;
+            MyCmd.Parameters.Add("@NumberOfChildren;", SqlDbType.Int).Value = employeeDetail.NumberOfChildren;
+            MyCmd.Parameters.Add("@IsDeleted;", SqlDbType.Int).Value = employeeDetail.IsDeleted;
             MyCmd.Parameters.Add("@Results", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
 
 
